dca2630853a958d2265fe1af6e4e6197568eefc0
Wouter Vernaillen <wouter@vernaillen.com>
2024-08-10 09:44:09 +0200

    moved existing components and queries

---
+++ src/runtime/components/blocks/CoreParagraph.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { CoreParagraph } from '#wpnuxt/blocks'
+
+defineProps<{
+  block: CoreParagraph
+}>()
+</script>
+
+<template>
+  <div>
+    <p v-sanitize="block?.attributes?.content" />
+  </div>
+</template>
---
+++ src/runtime/components/blocks/EditorBlock.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { EditorBlock } from '#wpnuxt/blocks'
+
+defineProps<{
+  block: EditorBlock
+}>()
+</script>
+
+<template>
+  <p v-sanitize="block.renderedHtml" />
+</template>
